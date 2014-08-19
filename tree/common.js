b82a3d8b43e035662f96d6541d602cabf023a9c4
dima <dima@dima.(none)>
2014-08-19 11:20:24 +0300

    points

--- tree/common.js
+++ tree/common.js
@@ -1,9 +1,8 @@
 
-var camera, scene, renderer, pano, point_group, camera_pano_x, camera_pano_y, domEvents, koef = 3;
+var camera, scene, renderer, pano, share_plus, domEvents, koef = 3, point_array;
 var points_array_group = new Array(), real_array = new Array();
 
 var isUserInteracting = false,
-    lite = 0x555555,
     lon = 0,
     acx, R = 0, L = 0,
     lat = 0,
@@ -39,13 +38,29 @@ animate();
  */
 function point_round(points){
     points_array_group = [];
-    var k = 0;
+    point_array = [];
+    var k = 0, line = 0;
+
     for(var i = 0; i < points.length; i++){
-        if(points[i]['code'] == 1){
-            points_array_group[k]=points[i];
-            k++;
+        if(points[i]['namber'] == point){
+            point_array = points[i];
+
         }
     }
+
+    for(var i = 0; i < points.length; i++){
+        if(points[i]['code'] == point_array['code']){
+            points_array_group[k]=points[i];
+            k++;
+            var l = Math.pow(point_array['xcoord'] - points[i]['xcoord'],2)+Math.pow(point_array['ycoord'] - points[i]['ycoord'],2);
+            if(l>line){
+                line = l;
+            }
+alert(l);
+        }
+    }
+    share_plus = Math.sqrt(line);
+
 }
 
 /**
@@ -80,16 +95,18 @@ function doOnLoad(){
  * @returns {string}
  */
 function sizePanoView (){
-    var panosize = 'x';
+    var panosize = 'l';
 
     if(window.innerWidth < 1400){
-        panosize='m';
+        panosize='l';
     }
 
     if(tablet){
-        panosize='l';
+        panosize='m';
     }
+
     return panosize;
+
 }
 
 /**
@@ -110,8 +127,10 @@ function viewPoints(){
         real_array[i].position.z = points_array_group[i]['ycoord']*koef;
 
         scene.add( real_array[i] );
+        real_array[i]['namber'] = points_array_group[i]['namber'];
 
     }
+
 
     for(var i = 0; i < real_array.length; i++){
 
@@ -127,7 +146,7 @@ function viewPoints(){
 
         domEvents.addEventListener(real_array[i], 'click', function(event){
 
-            clickToNewPoint(event.target.id);
+            clickToNewPoint(event.target.namber);
         }, true);
         console.debug(real_array[i]);
     }
@@ -138,7 +157,13 @@ function viewPoints(){
  * @param mewPoint
  */
 function clickToNewPoint(newPoint){
-    alert(newPoint);
+
+    point = newPoint;
+
+    point_round(points_array);
+    init();
+    viewPoints();
+
 }
 
 /**
@@ -146,16 +171,17 @@ function clickToNewPoint(newPoint){
  */
 function init() {
 
-    var container, mesh;
+    var container = [], mesh = [];
 
     container = document.getElementById( 'container' );
 
     camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 1, 1100 );
     camera.target = new THREE.Vector3( 100, 40, 30 );
     camera.position.y = 15;
-    camera.position.x = -15;
-    camera.position.z = 0;
+    camera.position.x = (point_array['xcoord']*koef)-15;
+    camera.position.z = point_array['ycoord']*koef;
     scene = new THREE.Scene();
+
 
     //создание глобальной площади - тоесть отрисовка всех доступных точек
 /*
@@ -169,7 +195,7 @@ function init() {
     oknoreal.position.x = 0;
     oknoreal.position.z = 0;
 */
-    var geometry = new THREE.SphereGeometry( 200, 100, 100 );
+    var geometry = new THREE.SphereGeometry( (share_plus*koef) + 100, 60, 40 );
 
     renderer = doOnLoad();
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -185,9 +211,12 @@ function init() {
     } );
 
     mesh = new THREE.Mesh( geometry, material );
-
+    mesh.position.y = 0;
+    mesh.position.x = point_array['xcoord']*koef;
+    mesh.position.z = point_array['ycoord']*koef;
     //ставим на сцену обьекты
     scene.add( mesh );
+
 
 
 
@@ -273,13 +302,12 @@ function onDocumentMouseMove( event ) {
 
         if(tablet){
 
-            alert(event.targetTouches[0].pageX);
-            lon = ( onPointerDownPointerX - event.targetTouches[0].pageX ) * 0.1 + onPointerDownLon;
-            lat = ( event.targetTouches[0].pageY - onPointerDownPointerY ) * 0.1 + onPointerDownLat;
+            lon = ( onPointerDownPointerX - event.targetTouches[0].pageX ) * 0.05 + onPointerDownLon;
+            lat = ( event.targetTouches[0].pageY - onPointerDownPointerY ) * 0.05 + onPointerDownLat;
 
         }else{
-            lon = ( onPointerDownPointerX - event.clientX ) * 0.1 + onPointerDownLon;
-            lat = ( event.clientY - onPointerDownPointerY ) * 0.1 + onPointerDownLat;
+            lon = ( onPointerDownPointerX - event.clientX ) * 0.05 + onPointerDownLon;
+            lat = ( event.clientY - onPointerDownPointerY ) * 0.05 + onPointerDownLat;
         }
 
     }
@@ -344,13 +372,12 @@ function update() {
 
 
     // вращение камеры
-    camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
+    camera.target.x = ((point_array['xcoord']*koef)+500) * Math.sin( phi ) * Math.cos( theta );
     camera.target.y = 500 * Math.cos( phi );
-    camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
+    camera.target.z = ((point_array['ycoord']*koef)+500) * Math.sin( phi ) * Math.sin( theta );
 
-    camera.position.x = -30 * Math.sin( phi ) * Math.cos( theta );;
-    camera.position.y = -30 * Math.cos( phi );
-    camera.position.z = -30 * Math.sin( phi ) * Math.sin( theta );
+    camera.position.x = point_array['xcoord']*koef;
+    camera.position.z = point_array['ycoord']*koef;
 
     camera.lookAt( camera.target );
 
